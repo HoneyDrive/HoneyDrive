98f9012652ab732bf40fe98c17276ad77f3b92f8
haakohu <Haak-hu@hotmail.com>
2016-03-17 21:11:45 +0100

    Added more test data

--- src/Helpers/GraphMain.java
+++ src/Helpers/GraphMain.java
@@ -21,7 +21,7 @@ public class GraphMain extends Application {
         IDataReader reader;
 
         primaryStage.show();
-        reader = new ReadFromOpenXCFile("src/metrics/TestData/data3.json");
+        reader = new ReadFromOpenXCFile("src/metrics/TestData/agressive_driving.json");
         reader.startReading();
 
     }
