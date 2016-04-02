f51a716c4551df471de02179d6cb9e72cd681486
haakohu <Haak-hu@hotmail.com>
2016-04-02 15:48:55 +0200

    ..

--- src/trips/DrivingHistory.java
+++ src/trips/DrivingHistory.java
@@ -78,8 +78,9 @@ public class DrivingHistory {
 	public int averageKmPerTrip(){
 		return ((int) history.stream().mapToDouble(Trip::getTotalDistance).sum())/history.size();
 	}
-	public double averageFuelPerTrip(){
-		return (history.stream().mapToDouble(Trip::getFuelBurntPer10Km).sum())/history.size();
+	public double averageFuelPerTrip() {
+		return (history.stream().mapToDouble(Trip::getFuelBurntPer10Km).sum()) / history.size();
+	}
 	public void generateMockData(){
 		addTrip(new Trip(new Score(5),50,50,5));
 		addTrip(new Trip(new Score(4),60,30,4));
